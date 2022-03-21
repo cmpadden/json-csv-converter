9aa4ce35c7a994f16352a62496c1c88bcff927e9
Colton Padden <colton.padden@fastmail.com>
2022-03-21 12:11:55 -0400

    render file content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,62 @@
-import { ReactElement, FC } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import FileUpload from "../components/FileUpload";
+import React, { ReactElement, FC, useEffect } from "react";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
 import { Box, Typography } from "@mui/material";
 
-import UploadFileIcon from "@mui/icons-material/UploadFile";
+interface FileInfoProps {
+  file: File;
+}
 
-import FileUpload from "../components/FileUpload";
+const FileInfo: FC<FileInfoProps> = ({ file }): ReactElement => {
+  const [fileContent, setFileContent] = React.useState<
+    string | ArrayBuffer | null
+  >("");
+
+  useEffect(() => {
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = () => {
+        setFileContent(reader.result);
+      };
+    }
+  });
+
+  return (
+    <Card sx={{ width: 450 }}>
+      <CardContent>
+        <Typography sx={{ mb: 2 }} variant="h5" component="div">
+          {file.name} ({file.type})
+        </Typography>
+        <Typography color="text.secondary">
+          Last Modified {new Date(file.lastModified).toLocaleString()}
+        </Typography>
+        <Typography color="text.secondary">{file.size} bytes</Typography>
+        <Typography
+          color="text.primary"
+          sx={{
+            border: "1px lightgrey solid",
+            p: 2,
+            mt: 2,
+            height: "250px",
+            overflowY: "scroll",
+          }}
+        >
+          {fileContent}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
 
 const Home: FC<any> = (): ReactElement => {
+  const [file, setFile] = React.useState<File | null>(null);
+
   const fileHandler = (files: FileList) => {
-    console.log(files);
+    const file = files.item(0);
+    setFile(file);
   };
 
   return (
@@ -18,6 +67,7 @@ const Home: FC<any> = (): ReactElement => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        p: 2,
       }}
     >
       <div>
@@ -25,23 +75,30 @@ const Home: FC<any> = (): ReactElement => {
           Drag-and-drop a file into the box below to convert it to another
           filetype. Supported filetypes include XML, CSV, and JSON.
         </Typography>
-        <FileUpload
-          fileHandler={fileHandler}
-          fileLimit={1}
-          allowedExtensions={["xml"]}
+        <Box
+          sx={{
+            mb: 2,
+          }}
         >
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
+          <FileUpload
+            fileHandler={fileHandler}
+            fileLimit={1}
+            allowedExtensions={["xml"]}
           >
-            <UploadFileIcon sx={{ fontSize: 45, mr: 1 }} />
-            <Typography variant="h5">Drag a File Here...</Typography>
-          </Box>
-        </FileUpload>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <UploadFileIcon sx={{ fontSize: 45, mr: 1 }} />
+              <Typography variant="h5">Drag a File Here...</Typography>
+            </Box>
+          </FileUpload>
+        </Box>
+        {file && <FileInfo file={file} />}
       </div>
     </Box>
   );
